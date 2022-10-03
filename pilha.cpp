f954cbfcd928169707435ecb6bf19334fb330be7
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2022-10-03 11:48:43 -0300

    Add files via upload
    Terminar de implementar as operações de pilha
    (remoção inser

---
+++ pilha.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+struct id{
+    int chave;
+    int valor;
+};
+
+
+int main (void){
+    id vetor[3] = {7851, 1,3527,2,9363,3};
+    cout << vetor[2].valor <<endl;
+
+    
+    /* data */
+}
