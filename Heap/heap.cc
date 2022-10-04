296e744455ebcf136a8803d20ab48af59482cf34
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2022-10-04 16:12:07 -0300

    Update heap.cc

--- Heap/heap.cc
+++ Heap/heap.cc
@@ -4,35 +4,38 @@
 
 using namespace std;
 
-void HeapSort(int a[],int b[],int c[]){
-    
-    for(int i=0;i < 3;){
-
-        for (int j = 0; j < 5;){
-            if(a[i]<b[j]){
-            printf("%d \n",a[i]);
-            if(i < 3){i++;}
-            }
-            else{
-            printf("%d \n",b[j]);
-            j++;
+void HeapSort(int a[], int b[], int c[])
+{
+    for (int i = 0; i < 3;)
+    {
+        for (int j = 0; j < 6;)
+        {
+            for (int k = 0; k < 10;)
+            {
+                if (a[i] < b[j] && i < 4 /*tamanho vetor A*/)
+                {
+                    c[k] = a[i];
+                    printf("%d \n", c[k]);
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    c[k] = b[j];
+                    printf("%d \n", c[k]);
+                    j++;
+                    k++;
+                }
             }
         }
-        
-        
-    }        
+    }
 }
-    
-
-
-
-
-int main(void){
-    int veta[] = {0,3,4,7};
-    int vetb[] = {1,2,5,6,8,9};
+int main(void)
+{
+    int veta[] = {0, 3, 4, 7};
+    int vetb[] = {1, 2, 5, 6, 8, 9};
     int vetc[10];
 
-HeapSort(veta,vetb,vetc);
+    HeapSort(veta, vetb, vetc);
 
-    //
 }
