a3981fc9a0e115c187254f127ccf495c2c6413b8
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2022-10-06 12:41:08 -0300

    Update pilha.cpp

--- pilha.cpp
+++ pilha.cpp
@@ -2,10 +2,25 @@
 
 using namespace std;
 
-struct id{
+struct id
+{
     int chave;
     int valor;
 };
+
+void adiciona(int x, int vet[])
+{
+    int i;
+
+
+    /*if (i < 5)
+    {
+        vet[i] = x;
+        
+        printf("%d",i,"%d",vet[i]);
+        i++;
+    }*/
+}
 
 /*
 int x;
@@ -14,10 +29,37 @@ int x;
     cout << "vc tem "<<x<< "anos \n";
 */
 
-int main (void){
-    id vetor[3] = {7851, 1,3527,2,9363,3};
-    cout << vetor[2].valor <<endl;
-
+int main(void)
+{
+    id vetor[3] = {7851, 1, 3527, 2, 9363, 3};
+    cout << vetor[2].valor << endl;
+    int x;
+    int para = 1;
+    int digito;
+    int vet[5];
+    cout << " 1 - Adiciona \n 2 - exclui\n 3 - sair\n"
+         << endl;
+    cin >> digito;
+    while(para == 1){
+    switch (digito)
+    {
+    case 1:
+        //add
+        cout << "valor a ser adicionado" << endl;
+        cin >> x;
+        adiciona(x, vet);
+        break;
+    case 2:
+        // remove
+        cout << "not implemented yet";
+        break;
+    case 3:
+        cout << " tchau! ";
+        para = 0;
+        break;
+    default: 
+        cout << " ? ? ? ";
+    }
+    }
     
-    /* data */
 }
