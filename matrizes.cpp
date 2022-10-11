c2dc521d79c715f5f75fa6076570fe678e2df481
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2022-10-11 18:21:04 -0300

    Update matrizes.cpp

--- matrizes.cpp
+++ matrizes.cpp
@@ -16,12 +16,12 @@ int main(int argc, char** argv){
         {0,1,2,3,4}
     };
     int m3[3][5];
-/*
+/* matrix esperada!
 0	6	12	18	24
 0	6	12	18	24
 0	6	12	18	24
 */
-    
+    // COM PONTEIROS SERIA ALGO ASSIM +-:
     // int *p = &m1[0][0], *q = &m2[0][0], *r = &m3[0][0];
     //  int *p1, *q1, *r, *p2, *q2;
     //  r = &m3[0][0];
@@ -30,37 +30,25 @@ int main(int argc, char** argv){
     //     for(q1=&m2[0][0];q1 < &m2[0][0]+5; q1++){
     //        for(p2=p1,q2=q1;p2<p1+4;p2++,q2+5)
     //        *r = *r + *p2 * *q2
-
-
-    //     }
+    //     }          
     // }
-    
-
-
-printf("\n\n%d\n\n",m2[1][2]*m1[1][0]);
-
-    for (int i = 0; i < 3;){  
-        for (int j = 0; j < 5;){
-            m3[i][j] = ((m1[i][j])*(m2[i][j])) +  ((m1[i][j+1])*(m2[i+1][j])) + ((m1[i][j+2])*(m2[i+2][j])) + ((m1[i][j+3])*(m2[i+3][j]));
-            printf(" %d ", m3[i][j]);
-            j++;
-        }
-        printf("\n");
-        i++;
-        j = 0;
-    }
-    
-
-    
-    // for ()
+        // for ()
     // {
     //     *r = *p + *q;
     //     cout << " " << *r;
     //     //printf("%d \n",*r);
     // }
-    
 
-
+    for (int i = 0; i < 3; i++){  
+        for (int j = 0; j < 5; j++){
+            m3[i][j] = ((m1[i][0])*(m2[0][j])) 
+            +  ((m1[i][1])*(m2[1][j])) 
+            + ((m1[i][2])*(m2[2][j])) 
+            + ((m1[i][3])*(m2[3][j]));
+            printf(" %d ", m3[i][j]);
+    }
+    printf("\n");
+}
 
     return 0;
 }
