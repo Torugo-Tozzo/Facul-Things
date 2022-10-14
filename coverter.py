2b44c04893c867174f6864b41be186a4186eb3ef
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2022-10-14 14:56:03 -0300

    Update coverter.py

--- coverter.py
+++ coverter.py
@@ -7,14 +7,16 @@ import eyed3
 
 link = input("Link ou Url : ")
 yt = YouTube(link)
+base_dir = input("Diretorio de download:")
 
 print(yt.title + "\n\nBaixando...")
 
 yd = yt.streams.get_audio_only()
 yd.download(filename ='generico.mp4')
 image_filename = wget.download(yt.thumbnail_url)
+#my base_dir:C:/Users/Karat/Documents/PastaProgramas/.vscode/
+#base_dir = "C:/Users/Karat/Documents/PastaProgramas/.vscode/"
 
-base_dir = "C:/Users/Karat/Documents/PastaProgramas/.vscode/"
 mp4_file = base_dir + 'generico.mp4'
 mp3_file = base_dir + 'generico.mp3'
 
@@ -28,9 +30,9 @@ f.initTag()
 f.tag.images.set(3, open("sddefault.jpg", 'rb').read(), 'image/jpeg')
 f.tag.save()
 
-oldname = "C:/Users/Karat/Documents/PastaProgramas/.vscode/generico.mp3"
-newname = "C:/Users/Karat/Documents/PastaProgramas/.vscode/" + yt.title+".mp3"
+oldname = base_dir + "generico.mp3"
+newname = base_dir + yt.title+".mp3"
 os.rename(oldname, newname)
-os.remove("C:/Users/Karat/Documents/PastaProgramas/.vscode/sddefault.jpg")
+os.remove(base_dir + "sddefault.jpg")
 
 print("completo! "+ mp3_file +" baixado com sucesso!")
