8dc21a3754be6e77ef53c38b76d7e912da1dd4f1
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2022-10-20 12:31:55 -0300

    Update ponteirostruct.cpp

--- atividade1/ponteirostruct.cpp
+++ atividade1/ponteirostruct.cpp
@@ -21,24 +21,41 @@ typedef struct Veiculo{
 Veiculo *bd[50];
 
 
+int r;
 
-void busca(Veiculo *bd[],int tam,string exemplo){
+int busca(Veiculo *bd[],int tam,string exemplo){
     //BUSCA    
     for(int j = 0; j < tam; j++ ){
         //oq vai ser buscado (placa)
         if (bd[j]->placa == exemplo)
         {
-            
-            cout << "\n encontrado na posição = " << j << " ! \n";
-            
+            r = j;
+            cout << "\n encontrado na posição = " << j << ".\n";
         }  
     }
-
+return r;
 }
-void exclui(Veiculo *bd[],int tam,int j){
-    bd[j] = NULL;
+void exclui(Veiculo *bd[],int tam,string ex){
+    bd[busca(bd,tam,ex)];
+    bd[r] = NULL;
+    for (int i = r; i < tam; i++)
+    {
+        bd[i] = bd[i+1];
+    }
+    //delete(bd[tam-1]);
     //delete(bd[j]);
 }
+int insere(Veiculo *bd[], int tam){
+    for (int i = 0; i < tam ; i++){
+        if (bd[i] == NULL){
+        // trocar bd[i] NULL por bd[1] CONTEUDO
+        i = tam;
+        tam++;   
+        }
+    }
+    return tam;
+}
+        
 
 
 int main(int argc, char**argv){
@@ -64,7 +81,6 @@ int main(int argc, char**argv){
             myfile >> bd[tam]->placa; 
         
         tam++;
-
         }
         myfile.close();
         for(int j = 0; j < tam; j++ ){
@@ -83,14 +99,22 @@ int main(int argc, char**argv){
         }
     for(int i = tam; i < 50;i++){
         bd[i] = NULL;
-        }
-
-    busca(bd, tam, "MAN7774");
+    }
 
 
-    exclui(bd,tam,30);
-    cout << " valor: " << bd[30] << " \n";
+    string placa = "JHE7846";
 
+    busca(bd, tam, placa);
+    exclui(bd,tam,placa);
+    //insere(bd,tam,pos);
+    cout << "ano do carro excluído " << bd[29]->ano << ".\n";
+    cout << "ano do carro excluído " << bd[30]->ano << ".\n"; 
+
+    cout << "ano do carro excluído " << bd[29] << ".\n";
+    cout << "ano do carro excluído " << bd[30] << ".\n";
+    cout << "ano do carro excluído " << bd[31] << ".\n";
+    // 2015
+    cout << "tam = " << tam <<"\n";
     }
     else
          cout << "unable to open the file";
