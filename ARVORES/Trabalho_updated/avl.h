5b3d3aa99f9ba2bcf5ea93f002ed7579b553a66e
Victor H. T. Filho <hugovtotal@gmail.com>
2023-01-23 20:46:38 -0300

    Trabalho da arvore-carros-Bressan

---
+++ ARVORES/Trabalho_updated/avl.h
@@ -0,0 +1,22 @@
+#pragma
+
+// Nós da arvore
+struct Node {
+  int value;
+  int height; // altura nó
+  Node *left;
+  Node *right;
+};
+
+int height(Node *node);
+int getBalance(Node *node);
+Node *rightRotate(Node *y);
+Node *leftRotate(Node *x);
+Node *insertAVL(Node *node, int value);
+void inorder(Node *root);
+void preorder(Node *root);
+void postorder(Node *root);
+Node* minValueNode(Node* node);
+Node* deleteNode(Node* root, int value);
+void printTree(Node* node, int level);
+
